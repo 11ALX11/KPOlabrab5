7573ef4618399d87093e137bb2d0713ef5e98074
Aleksey <11_alx_11@ro.ru>
2022-04-21 11:43:31 +0300

    task 3: map (hesh-table)

--- Z3/main.cpp
+++ Z3/main.cpp
@@ -1,9 +1,54 @@
 #include <iostream>
+#include <map>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    string data;
+    map<string, string> visits;
+
+    //file input
+    fstream input;
+    input.open("input", ios::in);
+
+    getline(input, data);
+    while (data.length() > 0) {
+        string name, date;
+
+        //parsing string ( name scndName - date )
+        int cntSp = 0, date_begining;
+        for (int i=0; i<data.length(); i++) {
+            if (data[i] == ' ') {
+                switch (cntSp) {
+                    case 1:
+                        //name from data
+                        name = data.substr(0, i);
+                        break;
+                    case 2:
+                        date_begining = i+1;
+                        break;
+                }
+                cntSp++;
+            }
+            if (i == data.length()-1) {
+                //date from data
+                date = data.substr(date_begining, i-date_begining+1);
+            }
+        }
+
+        //add visit to map
+        visits[name] = date;
+
+        //next string
+        getline(input, data);
+    }
+
+    //display visits
+    for (auto visit = visits.begin(); visit != visits.end(); visit++) {
+        cout << visit->first << " - " << visit->second << endl;
+    }
+
     return 0;
 }
